5be3071b34f66d917e77997c077b2cf3c4bbe09c
Chris Roy <chris@northswiss.com>
2021-08-28 09:29:30 +0200

    Add hero section and minor config

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout/layout"
+import SEO from "../components/layout/seo"
+
+function IndexPage() {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <a href="/">
+      <img src="/images/logos/northswiss-color.svg" alt="northswiss logo" />
+      </a>
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+    </Layout>
+  )
+}
+
+export default IndexPage
